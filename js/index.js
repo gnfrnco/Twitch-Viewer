c84f42a0d177aed9d7911c280b1c070335e0a4db
Gian Lazaro <gianlazaro@protonmail.com>
2016-11-08 15:25:23 -0800

    Add invalid streams filter

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(".button-collapse").sideNav();
-    var streamers = ['goldglove', 'proteqtv', 'c9sneaky', 'freaken_rican', 'lagmarine10', 'calebhart42', 'unlckyme', 'eyes1015', 'delordione', 'freecodecamp', 'food', "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    var streamers = ['freecodecamp', 'esl_sc2', 'comster404', 'ogamingsc2', 'cretetion', 'storbeck', 'habathcx', 'robotcaleb', 'noobs2ninjas', 'loltyler1', 'freaken_rican', 'brunofin'];
     var apiURL = "https://api.twitch.tv/kraken/streams?client_id=j6rg8fuur9h606pyrsy9t75rzh1tyqp&channel=" + streamers.join(',');   
     var user = '';
     console.log(apiURL);
@@ -8,42 +8,58 @@ $(document).ready(function(){
     $.getJSON(apiURL, function(streamData){       
         var online =[];
         var offline = [];
+        var deleted = [];
 
         for(var i=0; i<streamData._total; i++){
             var streamerName = streamData.streams[i].channel.display_name.toLowerCase();
             online.push(streamerName);}
-        for(var j=0; j<streamers.length;j++)
-            if(online.indexOf(streamers[j]) === -1) offline.push(streamers[j]);
+        for(var j=0; j<streamers.length;j++){
+            
+            if(online.indexOf(streamers[j]) === -1) {offline.push(streamers[j])};
+            var apiURL2 = 'https://api.twitch.tv/kraken/channels/' + streamers[j] + '?client_id=j6rg8fuur9h606pyrsy9t75rzh1tyqp';
+            // Used .ajax call because .getJSON does not have an error field to filter 404s, aka invalid users            
+            $.ajax({
+                url: apiURL2,
+                error: function(){
+                    deleted.push(streamers[j]);
+                },
+                async: false
 
+            });
+
+        }
         var onlineStatus = [];
         for (var a=0; a<streamData._total; a++){
         onlineStatus.push(streamData.streams[a].channel.status);
         }
 
-        getStreamer(streamers, online, onlineStatus);
-
-        $('.all').on('click', function(){ getStreamer(streamers, online, onlineStatus); });
-        $('.online').on('click', function(){ getStreamer(online, online, onlineStatus); });
-        $('.offline').on('click', function(){ getStreamer(offline, online, onlineStatus); });
+        getStreamer(streamers, online, onlineStatus, deleted);
+        $('.all').on('click', function(){ getStreamer(streamers, online, onlineStatus, deleted); });
+        $('.online').on('click', function(){ getStreamer(online, online, onlineStatus, deleted); });
+        $('.offline').on('click', function(){ getStreamer(offline, online, onlineStatus, deleted); });
     });
 });
 
-function getStreamer(streamType, onlineData, status){
+function getStreamer(streamType, onlineData, status, deleted){
     var html = '';
-    console.log(status);
+
     for(var q=0; q<streamType.length; q++){
         html += "<a class= 'collection-item' href='http://www.twitch.tv/" + streamType[q] +"' target='_blank'><span class='title'>" + streamType[q].toUpperCase() + "</span>";
 
-        for(var r=0; r<onlineData.length;r++){
-            if(onlineData.includes(streamType[q])){
-                if(onlineData[r] === streamType[q]){
-                    html += "<span class='secondary-content'>" + status[r] + "<span class='onlineIndicator'>&#9679;</span></span></a>";
-                }
+        for(var a=0; a<onlineData.length; a++){
+            if(streamType[q] === onlineData[a]){
+                 html += "<span class='secondary-content'>" + status[a] + "<span class='onlineIndicator'>&#9679;</span></span></a>";
             }
-            else {
-            html += "</a>";
-            }
+
+        }
+        for(var d=0;d<deleted.length; d++){
+            if(streamType[q] === deleted[d]){
+                html += "<span class='secondary-content'>" + "INVALID " + "<span class='errorIndicator'>&#9679;</span></span></a>";
+                $(this).css("border", "5px solid red");
+            }     
         }
     }
+
+
     $('.collection').html(html);
 }
