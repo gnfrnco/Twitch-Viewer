af331d78eee500296e464a7ab4117a28567ed1a7
Gian Lazaro <gianlazaro@protonmail.com>
2016-11-08 12:23:14 -0800

    Refactored js, mvp

--- js/index.js
+++ js/index.js
@@ -2,33 +2,48 @@ $(document).ready(function(){
     $(".button-collapse").sideNav();
     var streamers = ['goldglove', 'proteqtv', 'c9sneaky', 'freaken_rican', 'lagmarine10', 'calebhart42', 'unlckyme', 'eyes1015', 'delordione', 'freecodecamp', 'food'];
     var apiURL = "https://api.twitch.tv/kraken/streams?client_id=j6rg8fuur9h606pyrsy9t75rzh1tyqp&channel=" + streamers.join(',');   
-
+    var user = '';
     console.log(apiURL);
 
-    $.getJSON(apiURL, function(data){       
+    $.getJSON(apiURL, function(streamData){       
         var online =[];
         var offline = [];
 
-        for(var i=0; i<data._total; i++){
-            var streamerName = data.streams[i].channel.display_name.toLowerCase();
+        for(var i=0; i<streamData._total; i++){
+            var streamerName = streamData.streams[i].channel.display_name.toLowerCase();
             online.push(streamerName);}
         for(var j=0; j<streamers.length;j++)
             if(online.indexOf(streamers[j]) === -1) offline.push(streamers[j]);
-        
-        $('.all').on('click', function(){ getStreamer(streamers, data); });
-        $('.online').on('click', function(){ getStreamer(online, data); });
-        $('.offline').on('click', function(){ getStreamer(offline, data); });
-    });
 
+        var onlineStatus = [];
+        for (var a=0; a<streamData._total; a++){
+        onlineStatus.push(streamData.streams[a].channel.status);
+        }
+
+        getStreamer(streamers, online, onlineStatus);
+
+        $('.all').on('click', function(){ getStreamer(streamers, online, onlineStatus); });
+        $('.online').on('click', function(){ getStreamer(online, online, onlineStatus); });
+        $('.offline').on('click', function(){ getStreamer(offline, online, onlineStatus); });
+    });
 });
 
-function getStreamer(streamType, data){
+function getStreamer(streamType, onlineData, status){
     var html = '';
+    console.log(status);
     for(var q=0; q<streamType.length; q++){
-        html += "<a class= 'collection-item avatar'><img src='https://static-cdn.jtvnw.net/jtv_user_pictures/freaken_rican-profile_image-2de8e465b814688a-300x300.jpeg' class='circle'>" + streamType[q] + "<span class='title'>Climbing to diamond</span><br><p>hello</p></a>";
-        //html += "<p><div class='chip'>" + streamType[q] + "</div></p>";
+        html += "<a class= 'collection-item' href='http://www.twitch.tv/" + streamType[q] +"' target='_blank'><span class='title'>" + streamType[q].toUpperCase() + "</span>";
 
-        //https://api.twitch.tv/kraken/users/a93_?client_id=j6rg8fuur9h606pyrsy9t75rzh1tyqp <--- to get the user avatar
+        for(var r=0; r<onlineData.length;r++){
+            if(onlineData.includes(streamType[q])){
+                if(onlineData[r] === streamType[q]){
+                    html += "<span class='secondary-content'>" + status[r] + "<span class='onlineIndicator'>&#9679;</span></span></a>";
+                }
+            }
+            else {
+            html += "</a>";
+            }
+        }
     }
     $('.collection').html(html);
 }
